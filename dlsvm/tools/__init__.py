36836b1c00c935e8d7ffe8830f9a6b712fe6ae43
s-sakagawa <57160484+s-sakagawa@users.noreply.github.com>
2020-05-19 22:12:45 +0900

    Add tools of label conversion

--- dlsvm/tools/__init__.py
+++ dlsvm/tools/__init__.py
@@ -1,2 +1,5 @@
 from .builder import build_model
-from .label_converter import label_2_pn
+from .label_converter import (
+    label_2_pn,
+    label_2_onehot
+)
--- dlsvm/tools/label_converter.py
+++ dlsvm/tools/label_converter.py
@@ -7,3 +7,10 @@ def label_2_pn(data):
     onehot = np.eye(n_class)[data]
 
     return np.where(onehot == 1, 1, -1).astype(np.float32)
+
+
+def label_2_onehot(data):
+    data = data.astype(np.int32)
+    n_class = np.unique(data).size
+
+    return np.eye(n_class)[data]
