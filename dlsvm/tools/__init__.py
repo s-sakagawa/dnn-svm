2c165d50338df1a3468845357be9cbe50bb30027
s-sakagawa <57160484+s-sakagawa@users.noreply.github.com>
2020-05-24 22:13:06 +0900

    Add function of setting kernel

--- dlsvm/tools/__init__.py
+++ dlsvm/tools/__init__.py
@@ -1,4 +1,7 @@
-from .builder import build_model
+from .builder import (
+    build_model,
+    set_kernel
+)
 from .label_converter import (
     label_2_pn,
     label_2_onehot
--- dlsvm/tools/builder.py
+++ dlsvm/tools/builder.py
@@ -43,3 +43,36 @@ def build_model(
         return model
     else:
         return DnnSvm(model=model, penalty=penalty, n_class=n_class)
+
+
+def set_kernel(
+        model: DnnSvm,
+        kernel: str = 'linear',
+        degree: int = 3,
+        gamma=1.,
+        r=0.
+):
+    if kernel == 'linear':
+        model.loss = model.loss_l2_linear_svm
+
+    elif kernel == 'poly':
+        model.degree = tf.Variable(
+            np.array(degree).astype(np.float32), name='degree', trainable=False
+        )
+        model.gamma = tf.Variable(
+            np.array(gamma).astype(np.float32), name='gamma', trainable=False
+        )
+        model.r = tf.Variable(
+            np.array(r).astype(np.float32), name='r', trainable=False
+        )
+        model.loss = model.loss_l2_poly_svm
+
+    elif kernel == 'rbf':
+        model.gamma = tf.Variable(
+            np.array(gamma).astype(np.float32), name='gamma', trainable=False
+        )
+        model.loss = model.loss_l2_rbf_svm
+
+    else:
+        msg = f'Expected kernel [linear, poly, rbf], actual {kernel}'
+        raise ValueError(msg)
